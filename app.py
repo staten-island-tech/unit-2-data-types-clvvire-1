6f1095fa1367c9901cdfa09811d31d6fc93a06fa
clvvire <claireg6325@nycstudents.net>
2024-09-26 10:09:32 -0400

    2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#CHALLENGE 1
 """ # Start the program
 user_input = input("enter a sentence: ")
 
@@ -11,3 +12,36 @@ def count_words(sentence):
 word_count = count_words(user_input)
 print(f"The number of words in your sentence is: {word_count}") """
 
+#CHALLENGE 2
+""" x = 16
+if x % 2 == 0:
+    print("x is even")
+else: 
+    print("x is not") """
+
+#CHALLENGE 3
+""" service = input("how was the servie? ")
+if service == "bad":
+    print("0% tip")
+if service == "okay":
+    print("15% tip")
+if service == "good":
+    print("20% tip")
+if service == "great":
+    print("25% tip") """
+
+#CHALLENGE 4
+def find_factors(n):
+    if n < 1:
+        return "enter a number."
+    
+    factors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors.append(i)
+    
+    return factors
+
+# Example usage:
+number = 28
+print(f"The factors of {number} are: {find_factors(number)}")
